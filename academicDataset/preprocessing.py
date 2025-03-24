db66f3280f60a6f497e9f19ae3bfae8a57e423b5
Vladyslav Korenevskyi <25588519+tmbkoren@users.noreply.github.com>
2025-03-24 16:44:49 -0700

    replaced high and low with range

--- academicDataset/preprocessing.py
+++ academicDataset/preprocessing.py
@@ -8,6 +8,12 @@ print(df.isnull().sum())  # Check for missing values
 
 df = df.drop('Volume', axis=1)  # Drop the 'Volume' column
 
+df['Range'] = df['High'] - df['Low']  # Create a new column 'Range'
+df = df.drop(['High', 'Low'], axis=1)  # Drop the 'High' and 'Low' columns
+
+
+# Fill missing values
+
 # Generate a complete date range and forward-fill missing values
 df = df.asfreq('D', method='ffill')  # Forward-fill prices on non-trading days
 df = df[~df.index.duplicated(keep='first')] # Remove duplicate dates
